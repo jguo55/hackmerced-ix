4e8c8b24709a8f932de79ec3366a94dc9a9c1c47
James Zhang <jameszhang345358@gmail.com>
2024-03-09 11:05:33 -0800

    inprogress main.py

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
 #water optimization for irrigation
 from weatherData import weatherData
 
-w = weatherData(37.3645,-120.4254)
+while True:
+    try:
+        latitude = float(input("Enter your latitude: \n"))
+    except ValueError:
+        print("An error occured, please try again.")
+        continue
+    else:
+        break
+
+while True:
+    try:
+        longitude = float(input("Enter your latitude: \n"))
+    except ValueError:
+        print("An error occured, please try again.")
+        continue
+    else:
+        break
+
+
+while True:
+    try:
+        w = weatherData(latitude,longitude)
+    except:
+        print("An error occured, please try again.")
+        continue
+    else:
+        break
 
 print (w.getMinTemp())
 print(w.getMaxTemp())
