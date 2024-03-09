e8e774fe6e150cb7263889135ca9fd4d9a6b7c6e
James Zhang <jameszhang345358@gmail.com>
2024-03-09 13:23:48 -0800

    Created a database for crops

---
+++ crops.sql
@@ -0,0 +1,25 @@
+CREATE TABLE crops AS
+    SELECT "Tobacco" AS name, 500 AS need, 0.32 as initial, 0.73 as development, 1 as final UNION
+    SELECT "Maize"          , 650 AS need, 0.41 as initial, 0.787 as development, 1 as final
+    SELECT ""               ,  AS need,  as initial,  as development, 1 as final
+    SELECT ""               ,  AS need,  as initial,  as development, 1 as final
+    SELECT ""               ,  AS need,  as initial,  as development, 1 as final
+    SELECT ""               ,  AS need,  as initial,  as development, 1 as final
+    SELECT ""               ,  AS need,  as initial,  as development, 1 as final
+    SELECT ""               ,  AS need,  as initial,  as development, 1 as final
+    SELECT ""               ,  AS need,  as initial,  as development, 1 as final
+    SELECT ""               ,  AS need,  as initial,  as development, 1 as final
+    SELECT ""               ,  AS need,  as initial,  as development, 1 as final
+    SELECT ""               ,  AS need,  as initial,  as development, 1 as final
+    SELECT ""               ,  AS need,  as initial,  as development, 1 as final
+    SELECT ""               ,  AS need,  as initial,  as development, 1 as final
+    SELECT ""               ,  AS need,  as initial,  as development, 1 as final
+    SELECT ""               ,  AS need,  as initial,  as development, 1 as final
+    SELECT ""               ,  AS need,  as initial,  as development, 1 as final
+    SELECT ""               ,  AS need,  as initial,  as development, 1 as final
+    SELECT ""               ,  AS need,  as initial,  as development, 1 as final
+    SELECT ""               ,  AS need,  as initial,  as development, 1 as final
+    SELECT ""               ,  AS need,  as initial,  as development, 1 as final
+    SELECT ""               ,  AS need,  as initial,  as development, 1 as final
+    SELECT ""               ,  AS need,  as initial,  as development, 1 as final
+    SELECT ""               ,  AS need,  as initial,  as development, 1 as final
