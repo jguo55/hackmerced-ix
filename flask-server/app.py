ad83742b5db5bd99b31d6824f928cf2125fff9d8
jguo55 <148511131+jguo55@users.noreply.github.com>
2024-03-09 14:13:42 -0800

    user input forms

--- flask-server/app.py
+++ flask-server/app.py
@@ -1,35 +1,6 @@
 #water optimization for irrigation
 from weatherData import weatherData
 from flask import Flask, request
-
-''' while True:
-    try:
-        latitude = float(input("Enter your latitude: \n"))
-    except ValueError:
-        print("An error occured, please try again.")
-        continue
-    else:
-        break
-
-while True:
-    try:
-        longitude = float(input("Enter your latitude: \n"))
-    except ValueError:
-        print("An error occured, please try again.")
-        continue
-    else:
-        break
-
-
-while True:
-    try:
-        w = weatherData(latitude,longitude)
-    except:
-        print("An error occured, please try again.")
-        continue
-    else:
-        break '''
-
 res = 0
 
 app = Flask(__name__)
@@ -39,14 +10,14 @@ def result():
     try:
         w = weatherData(request.args.get('latitude'), request.args.get('longitude'))
     except:
-        return {
-            "result": "An error occurred. Ensure that your parameters are valid."
-        }
+        return 400
     #calculations here
 
     return{
         "result": res,
-        "cats": ["tabby", "leopard"]
+        "cats": ["tabby", "leopard"],
+        "latitude": w.getLatitude(),
+        "longitude": w.getLongitude()
         }
 
 
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,28 +6,32 @@ function App() {
 
   const [latitude, setLatitude] = useState(37)
   const [longitude, setLongitude] = useState(-120.4243)
+  const [crop, setCrop] = useState("nothing")
+  const [area, setArea] = useState(20)
 
+  const fetchData = async() => {
+      const res = await (await fetch(`/result?latitude=${latitude}&longitude=${longitude}&crop=${crop}&area=${area}`)).json()
+      setData(res)
+    }
 
-  useEffect(() => {
-    fetch(`/result?latitude=${latitude}&longitude=${longitude}`).then(
-      res => res.json()
-    ).then(
-      data => {
-        setData(data)
-        console.log(data)
-      }
-    )
-  },[])
+  function checkResponse(data){
+    if (typeof data.latitude === "undefined"){
+      return <p>Bad Input</p>
+    }
+    else {
+      return <p>{data.latitude}</p>
+    }
+  }
 
   return (
     <div className="App">
-      {(typeof data.cats === 'undefined') ? (
-        <p>Loading...</p>
-      ): (
-          <p>{data.result}</p>
-        )}
+      <input className='latitude' required="required" placeholder='Enter latitude' value = {latitude} onChange={e=>setLatitude(e.target.value)}/>
+      <input className='longitude' required="required" placeholder='Enter longitude' value = {longitude} onChange={e=>setLongitude(e.target.value)}/>
+      <button onClick={fetchData} type="submit">Generate</button>
+      {checkResponse(data)}
     </div>
   )
 }
+
 
 export default App
