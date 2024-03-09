1307012aced687fd4318d4b6a7e9b4ea262d6bfe
James Zhang <jameszhang345358@gmail.com>
2024-03-09 11:33:46 -0800

    fix flask server

--- flask-server/app.py
+++ flask-server/app.py
@@ -3,11 +3,40 @@ from weatherData import weatherData
 from flask import Flask
 
 
-w = weatherData(37.3645,-120.4254)
+#water optimization for irrigation
+from weatherData import weatherData
+
+while True:
+    try:
+        latitude = float(input("Enter your latitude: \n"))
+    except ValueError:
+        print("An error occured, please try again.")
+        continue
+    else:
+        break
+
+while True:
+    try:
+        longitude = float(input("Enter your latitude: \n"))
+    except ValueError:
+        print("An error occured, please try again.")
+        continue
+    else:
+        break
+
+
+while True:
+    try:
+        w = weatherData(latitude,longitude)
+    except:
+        print("An error occured, please try again.")
+        continue
+    else:
+        break
 
 print (w.getMinTemp())
 print(w.getMaxTemp())
-print(w.getCity())
+
 
 res = 0
 
